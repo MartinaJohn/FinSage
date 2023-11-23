eb3ab4af0201ff39ecfc082ab650ddf73fff869f
Suzan Dsouza <suzandsouza1505@gmail.com>
2023-11-23 18:52:44 +0530

    Completed forecaster page with the results printed

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pickle
 
 import numpy as np
 
+from gradio_client import Client
+client = Client("https://fingpt-fingpt-forecaster.hf.space/--replicas/vzfr2/")
+
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
 
 Pkl_Filename = "rf_tuned.pkl" 
@@ -14,7 +17,7 @@ def hello_world():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST','GET'])
+@app.route('/calc', methods=['POST','GET'])
 def predict():
     features = [int(x) for x in request.form.values()]
 
@@ -24,12 +27,23 @@ def predict():
     pred = model.predict(final)[0]
     print(pred)
 
-    
+    # render_template('home.html',result=pred)
     if pred < 0:
         return render_template('op.html', pred='Error calculating Amount!')
     else:
         return render_template('op.html', pred='Expected amount is {0:.3f}'.format(pred))
 
+
+@app.route('/forecaster', methods=['GET', 'POST'])
+def index():
+     result = None
+     if request.method == 'POST':
+         ticker = request.form['ticker']
+         date = request.form['date']
+         n_weeks = int(request.form['n_weeks'])
+         use_latest_basic_financials = request.form['use_latest_basic_financials'] == 'on'
+         result = client.predict(ticker, date, n_weeks, use_latest_basic_financials, api_name="/predict")
+     return render_template('stockForecaster.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
